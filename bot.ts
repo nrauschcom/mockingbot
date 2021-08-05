6958f042f9b523be915619fbd3f4c45942b4984f
Nico Rausch <rausch.nico@gmail.com>
2021-08-05 10:23:02 +0200

    Implemented Simple Text Mocking

--- bot.ts
+++ bot.ts
@@ -1,7 +1,39 @@
 import {Telegraf} from "telegraf";
+import * as crypto from 'crypto';
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('1923930394:AAHikPkG_08r3rcQv4kpk2IxoXJTpolo3v8');
+
+function mockText(input: string): string {
+    return [...input].map((char, idx) => char[idx % 2 == 0 ? 'toUpperCase' : 'toLowerCase']()).join('');
+}
 
 bot.on('inline_query', async (ctx) => {
-    const query = ctx.inlineQuery.query?.trim();
+    try {
+        const query = ctx.inlineQuery.query?.trim();
+        const mocked = mockText(query);
+
+        await ctx.answerInlineQuery([
+            {
+                type: 'article',
+                id: 'mock-text-' + crypto.createHash('md5').update(query).digest('hex'),
+                title: 'Mock that Shit!',
+                description: mocked.substr(0, 150),
+                input_message_content: {
+                    message_text: mocked
+                }
+            },
+            /*{
+                type: 'photo',
+                id: 'mock-photo-' + crypto.createHash('md5').update(query).digest('hex'),
+                title: 'Spongebob Mock',
+                description: 'Generate a Spongebob Mock Meme on the Fly',
+                photo_url: 'https://imgflip.com/s/meme/Mocking-Spongebob.jpg',
+                thumb_url: 'https://imgflip.com/s/meme/Mocking-Spongebob.jpg'
+            }*/
+        ], {is_personal: false});
+    } catch(e) {
+        console.log(e);
+    }
 });
+
+bot.launch();
